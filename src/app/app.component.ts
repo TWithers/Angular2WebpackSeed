8b9608988b09c415b07e6011ed23641e5f5f65f1
Tim Withers <timothy.withers@gmail.com>
2016-06-23 14:50:39 -0700

    Initial commit of seed application.

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+@Component({
+    selector: 'my-app',
+    template: require('./app.component.html'),
+    styles: [require('./app.component.css')]
+})
+export class AppComponent {
+    constructor(){
+        console.log('hello world');
+    }
+}
